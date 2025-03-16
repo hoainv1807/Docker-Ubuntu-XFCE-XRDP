54eb50ee030cbc7dfdd1ef3f0bbd9bc25467df25
SleepyHead <53241974+techroy23@users.noreply.github.com>
2025-03-17 01:51:20 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-docker run -d --cap-add=SYS_ADMIN -p 50001:3389 -e USERNAME=TESTUSER -e PASSWORD=TESTUSER debian-xfce-xrdp
+docker run -d --name debian-1 \
+  --shm-size 2g \
+  --cpus 2 \
+  --restart always \
+  --cap-add=SYS_ADMIN \
+  -v ubuntu1_vol:/persistent \
+  -e USERNAME=TESTUSER -e PASSWORD=TESTUSER \
+  debian-xfce-xrdp
